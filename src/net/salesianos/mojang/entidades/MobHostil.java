d773d46708f267ca9a2bd63351c8294880de9786
AleVallejoGarcia <usr2075@salesianos-lacuesta.net>
2023-05-29 12:21:12 +0100

    feat & fix:(Creacion de la clase mobHostil y cambiada la privacidad del atributo salud de la clase Mob)

---
+++ src/net/salesianos/mojang/entidades/MobHostil.java
@@ -0,0 +1,24 @@
+package net.salesianos.mojang.entidades;
+import net.salesianos.mojang.interfaces.Combate;
+
+public class MobHostil extends Mob implements Combate {
+    protected int fuerza;
+
+    public MobHostil(int salud) {
+        super(salud);
+    }
+
+    @Override 
+    public int atacar() {
+        return this.fuerza;
+    }
+
+    public void recibirAtaque(int ataque) {
+        this.salud = this.salud - ataque;
+        System.out.println("El daño recibido ha sido de " + ataque);
+        System.out.println("La salud restante es " + this.salud);
+    }
+
+    public void Moverse() {}
+
+}
